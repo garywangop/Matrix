65753e25c51ee52d77d63249f69315a268fcae40
garywangop <garywangop@gmail.com>
2020-02-01 02:32:13 -0500

    Navigation view with action bar

--- app/src/main/java/com/laioffer/matrix/ControlPanel.java
+++ app/src/main/java/com/laioffer/matrix/ControlPanel.java
@@ -3,10 +3,15 @@ package com.laioffer.matrix;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
 import android.os.Bundle;
+import android.view.MenuItem;
 
 public class ControlPanel extends AppCompatActivity {
+
+    private DrawerLayout drawerLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +24,17 @@ public class ControlPanel extends AppCompatActivity {
         actionbar.setDisplayHomeAsUpEnabled(true);
         actionbar.setHomeAsUpIndicator(R.drawable.baseline_home_black_18dp);
 
+        drawerLayout = findViewById(R.id.drawer_layout);
     }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                drawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
 }
